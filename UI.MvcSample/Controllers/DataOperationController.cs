7cdfc72c5eb1f91925f2723c8689aea23eb68a80
Payam7682 <payam7682@yahoo.com>
2022-02-23 08:27:40 +1300

    Add project files.

---
+++ UI.MvcSample/Controllers/DataOperationController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UI.MvcSample.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UI.MvcSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataOperationController : ControllerBase
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly string databaseName = "\\database.json";
+
+        public DataOperationController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET: api/<DataOperationController>
+        [HttpGet]
+        public async Task<List<UserModel>> GetAsync()
+        {
+            // DEFINE THE PATH WHERE WE WANT TO SAVE THE FILES.
+            string sPath = _hostingEnvironment.WebRootPath + "\\Data";
+
+            List<UserModel> userModels = new List<UserModel>();
+            string database = await System.IO.File.ReadAllTextAsync(sPath + databaseName);
+            userModels = JsonConvert.DeserializeObject<List<UserModel>>(database);
+
+            if (userModels == null) userModels = new List<UserModel>();
+
+            return userModels;
+        }
+
+        // POST api/<DataOperationController>
+        [HttpPost]
+        public async Task PostAsync([FromBody] UserModel model)
+        {
+            // DEFINE THE PATH WHERE WE WANT TO SAVE THE FILES.
+            string sPath = _hostingEnvironment.WebRootPath + "\\Data";
+
+            if (!System.IO.Directory.Exists(sPath))
+                System.IO.Directory.CreateDirectory(sPath);
+
+            if (!System.IO.File.Exists(sPath + databaseName))
+                System.IO.File.Create(sPath + databaseName);
+
+            List<UserModel> userModels = new List<UserModel>();
+            string database = await System.IO.File.ReadAllTextAsync(sPath + databaseName);
+            userModels = JsonConvert.DeserializeObject<List<UserModel>>(database);
+
+            if (userModels == null) userModels = new List<UserModel>();
+
+            userModels.Add(model);
+
+            await System.IO.File.WriteAllTextAsync(sPath + databaseName, JsonConvert.SerializeObject(userModels));
+        }
+    }
+}
---
+++ UI.MvcSample/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using UI.MvcSample.Models;
+
+namespace UI.MvcSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<bool> IndexAsync([FromForm] UserModel model)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:44393/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    //GET Method
+                    HttpResponseMessage response = await client.PostAsJsonAsync("api/DataOperation", model);
+                    return response.IsSuccessStatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return false;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
